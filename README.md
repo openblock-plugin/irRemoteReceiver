2eea09e3d032857cde04a503954fd6fa6851b3ff
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:19:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# IR Remote Receiver
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/irRemoteReceiver?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-communication-lightgrey.svg)
+
+Receiving and decoding data in infrared carrier.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,75 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'irRemoteReceiver.name': 'IR Remote Receiver',
+            'irRemoteReceiver.description': 'Receiving and decoding data in infrared carrier.'
+        },
+        'ru': {
+            'irRemoteReceiver.name': 'IR Remote Receiver',
+            'irRemoteReceiver.description': 'Receiving and decoding data in infrared carrier.'
+        },
+        'zh-cn': {
+            'irRemoteReceiver.name': 'IR Remote Receiver',
+            'irRemoteReceiver.description': 'Receiving and decoding data in infrared carrier.'
+        },
+        'zh-tw': {
+            'irRemoteReceiver.name': 'IR Remote Receiver',
+            'irRemoteReceiver.description': 'Receiving and decoding data in infrared carrier.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'IRREMOTERECEIVER_CATEGORY': 'IR Remote Receiver',
+            'IRREMOTERECEIVER_INIT': 'init IR remote receiver pin %1',
+            'IRREMOTERECEIVER_DATAAVAILABLE': 'IR remote receiver data available?',
+            'IRREMOTERECEIVER_RECIVEDCOMMAND': 'IR remote recived command',
+            'IRREMOTERECEIVER_RESUME': 'IR remote resume recive'
+        },
+        'ru': {
+            'IRREMOTERECEIVER_CATEGORY': 'IR Remote Receiver',
+            'IRREMOTERECEIVER_INIT': 'init IR remote receiver pin %1',
+            'IRREMOTERECEIVER_DATAAVAILABLE': 'IR remote receiver data available?',
+            'IRREMOTERECEIVER_RECIVEDCOMMAND': 'IR remote recived command',
+            'IRREMOTERECEIVER_RESUME': 'IR remote resume recive'
+        },
+        'zh-cn': {
+            'IRREMOTERECEIVER_CATEGORY': 'IR Remote Receiver',
+            'IRREMOTERECEIVER_INIT': 'init IR remote receiver pin %1',
+            'IRREMOTERECEIVER_DATAAVAILABLE': 'IR remote receiver data available?',
+            'IRREMOTERECEIVER_RECIVEDCOMMAND': 'IR remote recived command',
+            'IRREMOTERECEIVER_RESUME': 'IR remote resume recive'
+        },
+        'zh-tw': {
+            'IRREMOTERECEIVER_CATEGORY': 'IR Remote Receiver',
+            'IRREMOTERECEIVER_INIT': 'init IR remote receiver pin %1',
+            'IRREMOTERECEIVER_DATAAVAILABLE': 'IR remote receiver data available?',
+            'IRREMOTERECEIVER_RECIVEDCOMMAND': 'IR remote recived command',
+            'IRREMOTERECEIVER_RESUME': 'IR remote resume recive'
+        }
+    }
+};
